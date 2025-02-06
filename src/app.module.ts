9539ea3a65b3203e35d7a49e0853c64f82a27967
eamv0066 <adesarrollo4@unico.com.co>
2025-02-06 08:03:19 -0500

    Cambios de configuracion para obtener servicios SOAP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsController } from './controllers/cats.controller';
 import { CatsService } from './services/cats.service';
 import { Cat, CatSchema} from './schemas/cat.schema';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
+import { SoapService } from './soap/soap.service';
+import { SoapController } from './soap/soap.controller';
 
 @Module({
   imports: [
+    HttpModule,
     MongooseModule.forRoot('mongodb://root:root@localhost:27017/', {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     }),
     MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
   ],
-  controllers: [CatsController],
-  providers: [CatsService],
+  controllers: [CatsController, SoapController],
+  providers: [CatsService, SoapService],
 })
 export class AppModule {}
